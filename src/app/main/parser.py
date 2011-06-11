d4c51f521f3047e4276810c3625236fded751aa8
alerion <alerion.um@gmail.com>
2011-06-11 12:40:09 +0300

    update parser for new document style

--- src/app/main/parser.py
+++ src/app/main/parser.py
@@ -16,6 +16,55 @@ Question has this structure:
 
 def parser(file):
     """
+    № 9, 0, 1, 1, 1, 90
+    Особлива частина кримінального права України включає в себе норми таких видів:
+    -одноразові;
+    -похідні;
+    +заохочувальні;
+    -про помилування;
+    -каральні.
+    """
+    doc = opendocx(file)
+
+    questions_data = []
+    data = []
+
+    for p in doc.xpath('/w:document/w:body/w:p', namespaces=nsprefixes):
+        t = p.xpath('w:r/w:t', namespaces=nsprefixes)
+        if bool(t):
+            data.append(u''.join([i.text for i in t]).replace(u'№', u'#').strip())
+        else:
+            len(data) and questions_data.append(data)
+            data = []
+
+    if len(data):
+        questions_data.append(data)
+       
+    questions = []
+
+    for qs_data in questions_data:
+        if qs_data[0].startswith(u'№') or qs_data[0].startswith(u'#'):
+            qs_data = qs_data[1:]
+  
+        data = {
+            'question': qs_data[0],
+            'answers': []
+        }
+        
+        for answer in qs_data[1:]:
+            answer = answer.strip()
+            data['answers'].append({
+                'text': answer[1:],
+                'correct': answer.startswith('+')
+            })
+        
+        if data['answers']:
+            questions.append(data)
+  
+    return questions
+
+def parser2(file):
+    """
     № 2, 1, 1, 1, 120
     Якщо одна сторона бере на себе обов’язок перед другою стороною вчинити певні дії або утриматися від них, а друга сторона наділяється лише правом вимоги, без виникнення зустрічного обов’язку щодо першої сторони, то договір вважається :
     1) двостороннім;
